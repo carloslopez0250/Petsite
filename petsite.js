5f4127792bdd656bde33711bfd77c63dbb4d6dbb
Carlos Lopez <carlos.lopez.0250@gmail.com>
2021-09-13 23:04:16 -0700

    petsite.js added

---
+++ petsite.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function myFunction() {
+    alert('Hello Pet Lovers!');
+}
+if(hourNow > 18){
+    // code block will be run if the first condition is true
+    greeting = 'Good evening, Class!';
+    // Good evening
+} else if(hourNow > 12)
+{
+    greeting = 'Good afternoon, Class!';
+}  else if(hourNow >= 0){
+    greeting = 'Good morning, Class!';
+} else{
+    greeting = 'Welcome!'
+
