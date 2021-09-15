1bcb37ab31683982ccd884628e5803946e8762ce
Carlos Lopez <carlos.lopez.0250@gmail.com>
2021-09-14 21:15:09 -0700

    functions added to JavaScript

--- petsite.js
+++ petsite.js
@@ -1,30 +1,32 @@
 'use strict'
-let favPet = prompt('What is your favorite pet?');
 
-if (favPet.toLowerCase() === 'all pets'){
-    alert('We love all pets too!')
+function favPet(){
+    let favPet = prompt('What is your favorite pet?');
+    if (favPet.toLowerCase() === 'all pets'){
+        alert('We love all pets too!');
+    }
 }
+function greeting(){
+    let greeting = '';
+    let today = new Date();
+    let hourNow = today.getHours();
+    // let greeting;
 
-
-
-const greeting = document.getElementById('greeting');
-let today = new Date();
-let hourNow = today.getHours();
-// let greeting;
-
-if (hourNow > 18) {
-    greeting.innerText = 'Good Evening Fellow Pet Lovers!';
-} else if (hourNow > 12) {
-    greeting.innerText = 'Good Afternoon Fellow Pet Lovers!';
-} else if (hourNow > 0) {
-    greeting.innerText = 'Good Morning Fellow Pet Lovers!';
-}  else {
-    greeting.innerText = 'Welcome!';
+    if (hourNow >= 18) {
+        greeting= 'Good Evening Fellow Pet Lovers!';
+    } else if (hourNow > 12) {
+        greeting= 'Good Afternoon Fellow Pet Lovers!';
+    } else if (hourNow > 0) {
+        greeting= 'Good Morning Fellow Pet Lovers!';
+    }  else {
+        greeting= 'Welcome!';
+    }
+    document.write(greeting);
 }
 
 function myFunction() {
     let person = prompt("Please enter your name", "Harry Potter");
-    if (person != null) {
+    if (person != null || person != '') {
       document.getElementById("demo").innerHTML =
       "Hello " + person + "! Welcome to our ever growing pet community";
     }
